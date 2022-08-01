61a7bacb7f9c7055e71cdb6cebbaa8c43c0d2389
Jan Koppe <post@jankoppe.de>
2022-08-01 16:44:38 +0200

    fix user datasource tests

--- internal/provider/data_source_user_test.go
+++ internal/provider/data_source_user_test.go
@@ -14,17 +14,12 @@ func TestAccDataSourceUser(t *testing.T) {
 			{
 				Config: testAccDataSourceUser,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet("data.aws-sso-scim_user.foo", "id")),
-			},
-			{
-				Config: testAccDataSourceUserFull,
-				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet("data.aws-sso-scim_user.foo", "id"),
-					resource.TestCheckResourceAttr("data.aws-sso-scim_user.foo", "display_name", "terraform-test-temporary-user-display_nameXX"),
-					resource.TestCheckResourceAttr("data.aws-sso-scim_user.foo", "user_name", "terraform-test-temporary-user-user_nameXX"),
-					resource.TestCheckResourceAttr("data.aws-sso-scim_user.foo", "family_name", "terraform-test-temporary-user-family_nameXX"),
-					resource.TestCheckResourceAttr("data.aws-sso-scim_user.foo", "given_name", "terraform-test-temporary-user-given_nameXX"),
-					resource.TestCheckResourceAttr("data.aws-sso-scim_user.foo", "email_address", "terraformtestXX@burda-forward.de"),
+					resource.TestCheckResourceAttr("data.aws-sso-scim_user.foo", "display_name", "terraform-test-permanent-user"),
+					resource.TestCheckResourceAttr("data.aws-sso-scim_user.foo", "user_name", "terraform-test-permanent-user"),
+					resource.TestCheckResourceAttr("data.aws-sso-scim_user.foo", "family_name", "permanent-user"),
+					resource.TestCheckResourceAttr("data.aws-sso-scim_user.foo", "given_name", "terraform-test"),
+					resource.TestCheckResourceAttr("data.aws-sso-scim_user.foo", "email_address", "terraformtest@burda-forward.de"),
 					resource.TestCheckResourceAttr("data.aws-sso-scim_user.foo", "email_type", "work"),
 				),
 			},
@@ -35,24 +30,5 @@ func TestAccDataSourceUser(t *testing.T) {
 const testAccDataSourceUser = `
 data "aws-sso-scim_user" "foo" {
   user_name = "terraform-test-permanent-user"
-}
-`
-
-const testAccDataSourceUserFull = `
-resource "aws-sso-scim_user" "foo" {
-  display_name = "terraform-test-temporary-user-display_nameXX"
-  user_name = "terraform-test-temporary-user-user_nameXX"
-  family_name = "terraform-test-temporary-user-family_nameXX"
-  given_name = "terraform-test-temporary-user-given_nameXX"
-  active = false
-	email_address = "terraformtestXX@burda-forward.de"
-	email_type = "work"
-}
-
-data "aws-sso-scim_user" "foo" {
-  user_name = "terraform-test-temporary-user-user_nameXX"
-	depends_on = [
-		aws-sso-scim_user.foo
-	]
 }
 `
