44ebb6b15f0eeb45b8d9bcf0c074944ee6adc870
Jon Davidson <44708571+davidsonjon@users.noreply.github.com>
2023-02-01 09:41:56 -0600

    chore: add test, fmt

--- internal/provider/resource_group_test.go
+++ internal/provider/resource_group_test.go
@@ -18,10 +18,24 @@ func TestAccResourceGroup(t *testing.T) {
 						"aws-sso-scim_group.foo", "id")),
 			},
 			{
+				Config: testAccResourceGroupUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("aws-sso-scim_group.foo", "id"),
+					resource.TestCheckResourceAttr("aws-sso-scim_group.foo", "display_name", "terraform-test-temporary-group2"),
+				),
+			},
+			{
 				Config: testAccResourceGroupExternal,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet(
 						"aws-sso-scim_group.external_group", "id")),
+			},
+			{
+				Config: testAccResourceGroupExternalUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("aws-sso-scim_group.external_group", "id"),
+					resource.TestCheckResourceAttr("aws-sso-scim_group.external_group", "display_name", "terraform-test-temporary-external-group2"),
+				),
 			},
 		},
 	})
@@ -33,9 +47,22 @@ resource "aws-sso-scim_group" "foo" {
 }
 `
 
+const testAccResourceGroupUpdate = `
+resource "aws-sso-scim_group" "foo" {
+  display_name = "terraform-test-temporary-group2"
+}
+`
+
 const testAccResourceGroupExternal = `
 resource "aws-sso-scim_group" "external_group" {
-  display_name = "ExampleGroup"
-  external_id = "e5a41517-bcd6-4b8b-8590-487ae996de44"
+  display_name = "terraform-test-temporary-external-group"
+  external_id  = "e5a41517-bcd6-4b8b-8590-487ae996de44"
+}
+`
+
+const testAccResourceGroupExternalUpdate = `
+resource "aws-sso-scim_group" "external_group" {
+  display_name = "terraform-test-temporary-external-group2"
+  external_id  = "e5a41517-bcd6-4b8b-8590-487ae996de44"
 }
 `
